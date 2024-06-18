6f99b8f00289826ab92cb51e2aa3089f86ba27a6
KhumoMogotlhe <160323213+KhumoMogotlhe@users.noreply.github.com>
2024-06-18 16:29:17 +0200

    Add files via upload
    if..else statements

--- exercise6.java
+++ exercise6.java
@@ -1,2 +1,27 @@
 public class exercise6 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        if (a < b) System.out.println("a is less than b");
+    }
+
 }
+
+/* Java Conditions and If Statements
+*
+* Less than: a < b
+* Less than or equal to: a <= b
+* Greater than: a > b
+* Greater than or equal to: a >= b
+* Equal to a == b
+* Not Equal to: a != b
+*
+* Java has the following conditional statements:
+
+- if to specify a block of code to be executed, if a specified condition is true
+- else to specify a block of code to be executed, if the same condition is false
+- else if to specify a new condition to test, if the first condition is false
+- switch to specify many alternative blocks of code to be executed
+
+*
+*/
