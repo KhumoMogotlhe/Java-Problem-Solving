8f7909627520536b956d4f2be30ce3f7293f11d0
KhumoMogotlhe <160323213+KhumoMogotlhe@users.noreply.github.com>
2024-07-02 14:51:48 +0200

    Converting a binary number to a decimal number

--- exercise13.java
+++ exercise13.java
@@ -1,2 +1,16 @@
 public class exercise13 {
+    static String binaryConversion(String binaryNumber){
+        if (binaryNumber.matches("[01]+")){
+            return "Error: Not a binary number.";
+        }
+        int decimalNumber = 0;
+        int length = binaryNumber.length() - 1;
+        for (int i = 0; i < length; i++){
+            if (binaryNumber.charAt(length - 1 - i) == '1'){
+                decimalNumber += Math.pow(2 , i);
+            }
+        }
+        return String.valueOf(decimalNumber);
+    }
+
 }
---
+++ out/production/Java-Problem-Solving/README.md
@@ -0,0 +1,3 @@
+# Java-Problem-Solving
+Introduction to Java
+https://www.w3resource.com/java-exercises/#google_vignette
